09382234fbb26274ebab77ebea681508e39db6aa
vivoge <vivoge@163.com>
2016-09-03 20:19:09 +0800

    router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/reg',function(req,res){
+ res.render('../views/user/reg')
 });
+
+router.post('/reg',function(req,res){
+  res.send('reg');
+});
+
+router.get('/login',function(req,res){
+  res.render('../views/user/login')
+});
+
+router.post('/login',function(req,res){
+  res.send('login');
+});
+
+router.get('/logout',function(req,res){
+  res.render('../views/user/logout');
+});
+
 
 module.exports = router;
