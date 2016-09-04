ce87220bbb3f9638f8cb638afcd03c96e7752c88
vivoge <vivoge@163.com>
2016-09-04 16:33:39 +0800

    app.engine('html',require('ejs').renderFile)

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/add',function(req,res){
+    res.render('../views/articles/articles')
+});
+
+router.post('/add',function(req,res){
+    res.send('articles')
+});
 
 
 
 
 
 module.exports = router;
+
