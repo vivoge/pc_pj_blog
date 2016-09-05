34f4093976d79fee77af84768a2ca00b7903d954
vivoge <vivoge@163.com>
2016-09-05 18:53:45 +0800

    bao

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ app.use(cookieParser());
 app.use(session({
   secret:'sange',
   resave:true,
-  saveUninitialized:true,
+  saveUninitialized:true/*,
   store:new MongoStore({
     db:'sange_blog',
     host:'127.0.0.1',
     port:27017
-  })
+  })*/
 
 }));
 app.use(function(req,res,next){
