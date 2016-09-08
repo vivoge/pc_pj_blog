f2b232f9687ea95fd186bdf96022d8ff4d867f5b
vivoge@163.com <r5n74674kc>
2016-09-08 12:55:16 +0800

    增删改

--- routes/articles.js
+++ routes/articles.js
@@ -15,31 +15,91 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage })
 
+
+
 router.get('/add',function(req,res){
     res.render('../views/articles/add')
 });
 
+router.get('/update/:id',function(req,res){
+    var _id = req.params.id;
+    articleModel.findById(_id,function(err,docs){
+        res.render('../views/articles/update',{docs:docs});
+    });
+});
+
+
+router.post('/update',upload.single('img'),function(req,res){
+    var article = req.body;
+    var _id = article._id;
+
+    if(_id){
+        var set = {title:article.title,content:article.content};
+        if(req.file){
+            set.img ='/images/'+req.file.filename;
+        }
+        articleModel.update({_id:_id},{$set:set},function(err,article){
+            if(err){
+                req.flash('error','更新文章失败');
+                res.redirect('back');
+            }else{
+                req.flash('success','更新文章成功');
+                res.redirect('/');
+            }
+        })
+
+    }
+
+
+});
+
+
 //中间件放置的是文件域的名字
 router.post('/add',upload.single('img'),function(req,res){
     var article = req.body;
+
     if(req.file){
         article.img ='/images/'+req.file.filename;
-        console.log('222',article.img);
     }
     var user = req.session.user._id;
     article.user = user;
     articleModel.create(article,function(err,article){
         if(err){
-            req.flash('error','发表文章失败');
+            req.flash('error',err);
             res.redirect('back');
         }else{
             req.flash('success','发表文章成功');
             res.redirect('/');
         }
-    })
+    });
+
+
+
+
 
 
 });
+
+router.get('/detail/:id',function(req,res){
+    var _id = req.params.id;
+    articleModel.findById(_id,function(err,docs){
+        res.render('../views/articles/detail',{docs:docs});
+    });
+});
+
+router.get('/delete/:id',function(req,res){
+    var deleteId = req.params.id;
+    articleModel.remove({_id:deleteId},function(err,result){
+        if(err){
+            req.flash('error',err);
+            res.redirect('back');
+        }else{
+            req.flash('success','删除文章成功');
+            res.redirect('/');
+        }
+    });
+});
+
 
 
 
